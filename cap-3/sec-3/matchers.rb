0e94eee0f00447e59d8206978d5b3ace2e1ec061
Hugo Barauna <hugo.barauna@plataformatec.com.br>
2013-06-08 15:21:47 -0300

    Add more matchers examples

--- cap-3/sec-3/matchers.rb
+++ cap-3/sec-3/matchers.rb
@@ -7,6 +7,103 @@ describe "Be matchers", "when actual is `true`" do
   it { should be }
 end
 
+describe "Equality matchers" do
+  let(:a) { "a" }
+
+  it "has equal, eql, be and eq matchers" do
+    expect(a).to equal(a)
+    expect(a).not_to equal("a")
+
+    expect(a).to eql("a")
+    expect(a).not_to eql("b")
+
+    expect(a).to be(a)
+    expect(a).not_to be("a")
+
+    expect(a).to eq(a)
+    expect(a).to eq("a")
+    expect(a).not_to eq("b")
+  end
+end
+
+describe "Array related matchers" do
+  subject(:array) { [1, 2, 3, 4] }
+
+  it "has a MatchArray matcher" do
+    expect(array).to match_array([4, 3, 2, 1])
+    expect(array).to match_array([1, 2, 3, 4])
+
+    expect(array).not_to match_array([1, 2, 3])
+    expect(array).not_to match_array([1, 2, 3, 4, 5])
+  end
+
+  it "has an Include matcher" do
+    expect(array).to include(1)
+    expect(array).to include(1, 2, 3)
+    expect(array).to include(1, 2, 3, 4)
+
+    expect(array).not_to include(0)
+    expect(array).not_to include(5)
+    expect(array).not_to include(5, 6, 7, 8)
+    expect(array).not_to include([1, 2, 3, 4])
+  end
+
+  it "has a StartAndEndWith matcher" do
+    expect(array).to start_with(1)
+    expect(array).to start_with(1, 2)
+    expect(array).not_to start_with(2)
+
+    expect(array).to end_with(4)
+    expect(array).to end_with(3, 4)
+    expect(array).not_to end_with(3)
+  end
+end
+
+describe "Hash related matchers" do
+  subject(:hash) { { a: 7, b: 5 } }
+
+  it "has an Include matcher" do
+    expect(hash).to include(:a)
+    expect(hash).to include(:a, :b)
+    expect(hash).to include(a: 7)
+    expect(hash).to include(b: 5, a: 7)
+
+    expect(hash).not_to include(:c)
+    expect(hash).not_to include(a: 11)
+    expect(hash).not_to include(a: 13, c: 11)
+    expect(hash).not_to include(:c, :d)
+  end
+end
+
+describe "String related matchers" do
+  subject(:string) { "hugo barauna" }
+
+  it "has a Match matcher" do
+    expect(string).to match(/hugo/)
+    expect(string).to match(/araun/)
+
+    expect(string).not_to match(/barao/)
+    expect(string).not_to match(/hugs/)
+  end
+
+  it "has an Include matcher" do
+    expect(string).to include("h")
+    expect(string).to include("hugo")
+    expect(string).to include("hugo", "bara")
+
+    expect(string).not_to include("barao")
+    expect(string).not_to include("hugs")
+  end
+
+  it "has a StartAndEndWith matcher" do
+    expect(string).to start_with("hugo")
+    expect(string).not_to start_with("barauna")
+
+    expect(string).to end_with("barauna")
+    expect(string).not_to end_with("hugo")
+  end
+end
+
 describe "Predicate matchers", "for an old ball" do
   subject(:ball) do
     ball = double("ball", old?: true )
@@ -15,22 +112,26 @@ describe "Predicate matchers", "for an old ball" do
   it { should be_old }
 end
 
-describe "BeComparedTo matchers", "for the `7` number" do
-  subject { 7 }
+describe "Number related matchers" do
+  describe "BeComparedTo matchers", "for the `7` number" do
+    subject { 7 }
 
-  it { should be == 7 }
-  it { should be < 10 }
-  it { should be > 1 }
-  it { should be <= 7 }
-  it { should be >= 7 }
-  it { should be ===  7}
+    it { should be == 7 }
+    it { should be < 10 }
+    it { should be > 1 }
+    it { should be <= 7 }
+    it { should be >= 7 }
+    it { should be ===  7}
 
-  describe "=~ operator" do
-    subject { "foo" }
+    describe "=~ operator" do
+      subject { "foo" }
 
-    it { should be =~ /foo/ }
-    it { should_not be =~ /bar/ }
+      it { should be =~ /foo/ }
+      it { should_not be =~ /bar/ }
+    end
   end
+
+  it "has a BeWithin matcher"
 end
 
 describe "Operator matchers", "for the `7` number" do

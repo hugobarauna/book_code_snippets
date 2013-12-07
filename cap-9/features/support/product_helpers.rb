288830080e26bf9d567827ae363f72bc6fecd1c8
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2013-12-07 12:51:54 -0200

    Small fixes to the support code

--- cap-9/features/support/product_helpers.rb
+++ cap-9/features/support/product_helpers.rb
@@ -1,10 +1,12 @@
 module ProductHelpers
   def buy_product(product_name)
     product = Product.find_by(name: product_name) or
-                Product.create(name: product_name)
+                Product.create!(name: product_name)
 
     visit "/produtos/#{product_name}"
 
     click_button 'Comprar'
   end
 end
+
+World(ProductHelpers)
